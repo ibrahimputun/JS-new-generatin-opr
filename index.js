8948ae571f208d13c095d82df4410ce9f782760d
ibrahimputun <ibrahimputun01@gmail.com>
2023-10-31 09:38:51 +0300

    eklemeler yapıldı

--- index.js
+++ index.js
@@ -113,3 +113,233 @@ team.forEach((person) => {
     console.log("------------")
 })
 
+
+//!----  FUNCTIONLARDA DESTRUC. KULLANIMI ----
+// function kisiOkuDeclaration () {
+// }
+const kisiOku = function () {
+    return {
+        no: "1234567890",
+        isim: "Ahmet",
+        soyisim: "Can",
+        is: "Kasap",
+        dilSayisi: 2,
+    }
+}
+console.log("KİSİ:", kisiOku());
+
+//* function'un dondurdugu obje dogrudan dest. yapilabilir
+let { no, isim, soyisim, dilSayisi } = kisiOku()
+
+dilSayisi++
+console.log(no, isim, dilSayisi)
+
+//? Parameter olarak objenin ve destr. kullanimi
+const data = {
+    id: "1",
+    brand: "Apple",
+    product: "Iphone15",
+    stock: 100,
+}
+
+const productPrint = (data) => {
+    console.log(`${data.brand}-${data["product"]}: ${data.stock}`)
+}
+
+const productPrintDestr = (data) => {
+    const { brand, product, stock } = data
+    console.log(`${brand}-${product}: ${stock}`)
+}
+
+//? Alternatif destr (Havada veya Yolda)
+const productPrintDestrV2 = ({ brand, product, stock }) => {
+    console.log(`${brand}-${product}: ${stock}`)
+}
+
+productPrint(data)
+productPrintDestr(data)
+productPrintDestrV2(data)
+
+
+//* =============================================
+//*  DESTRUCTURING (ARRAY)
+//* =============================================
+const people = ["Ali", "Veli", "Can", "Canan"]
+
+console.log(people[1])
+
+//? Array destr. sıra önemlidir.
+const [p1, p2, , p4] = people
+console.log(p4)
+//* ==============================================
+//*  REST (...)
+//* =============================================
+//? REST operatoru kullanici tarafindan girilen degerleri dizi
+//? icerisine konumlandirir. Cesitli kullanim alanlari vardir.
+
+//! 1- Bir dizi veya object'deki bazi degerlerden geri kalanlarini
+//!    ayri dizi yada objelere kopyalanmasini saglayabilir.
+
+//?REST: Array
+const araclar = ["ATV", "Kamyonet", "TIR", "Kamyon", "Araba"]
+const [a1, a2, ...geriKalanAraclar] = araclar
+console.log(geriKalanAraclar) //? ["Kamyonet", "TIR", "Kamyon"]
+
+//?REST: Object
+const veri = {
+    id: "1",
+    brand: "Apple",
+    product: "Iphone15",
+    stock: 100,
+}
+
+const { id, product, ...productStock } = veri
+console.log(productStock)
+
+//! 2- Bir fonksiyonun argumanlarini diziye cevirmek icin kullanilabilir.
+
+const sum = (a, b) => a + b
+
+//? REST (...) ile non-iterable olan sayilari iterable hale (diziye) cevirmiş olduk.
+const sumAll = (...numbers) => {
+    // console.log(numbers)
+    return numbers.reduce((acc, val) => acc + val)
+}
+
+console.log("SUM:", sum(2, 5, 6, 7, 8))
+console.log("SUM-ALL:", sumAll(2, 5, 6, 7, 8))
+console.log("SUM-ALL:", sumAll(3, 4, 5))
+
+//* =============================================
+//*  SPREAD (...)
+//* =============================================
+//? Spread operatoru ise iterables olan bir elemani bireysel
+//? degerler haline getirir.
+
+const ucanAraclar = ["helicopter", "drone", "ucak", "fuze"]
+const karaAracları = ["araba", "bisiklet", "marti"]
+
+const tasitlar = [ucanAraclar, karaAracları] //!nested array
+console.log(tasitlar)
+console.log(tasitlar[1][2])
+
+const tasitlarFlat = [...karaAracları, "hoverCraft", "gemi", ...ucanAraclar]
+console.log(tasitlarFlat)
+
+//?Ornek:
+const slogan = "Reinvent Your self"
+const sloganArray = [...slogan]
+console.log(sloganArray)
+
+//?Ornek:
+//? Spread ile bir iterable(array), non-iterable'a cevirlebilir.
+const sayilar = [2, 4, 5, 13, 56, 23]
+console.log(Math.max(...sayilar))
+
+const ciftler = [2, 4, 6]
+const tekler = [1, 3, 5]
+
+const yeniSayilar = ciftler //? Sig Kopyalama (Derin Kopyalama)
+
+console.log(yeniSayilar, ciftler)
+yeniSayilar.push(8)
+console.log(yeniSayilar, ciftler)
+
+//? Kopyalama Spread ile yapilirsa
+const yeniArraySpread = [...ciftler]
+console.log(yeniArraySpread, ciftler)
+
+//? Spread ile yapılan kopyalamada tam olarak sig kopyalama gerceklesmez. Dolayısiyla birisindeki degisiklik digerine yansımaz.
+//? ISTISNA: Eger dizide nesting (içiçe veri) varsa onlar icin sig kopyalama gibi calisir.
+yeniArraySpread.push(10)
+console.log(yeniArraySpread, ciftler)
+
+//? Tamamen Deep Kopyalama icin JSON.stringfy() - JSON.parse()
+
+//? Object copying
+
+const firstObj = { a: 1, b: 2, c: 3 }
+const secondObj = { a: 2, d: 3, c: 4 }
+
+const copiedFirstObj = { ...firstObj }
+console.log(copiedFirstObj)
+
+copiedFirstObj.a = 44
+
+//? Spread ile yapılan kopyalamada tam olarak sig kopyalama gerceklesmez. Dolayısiyla birisindeki degisiklik digerine yansımaz.
+//? ISTISNA: Eger dizide nesting (içiçe veri) varsa onlar icin sig kopyalama gibi calisir.
+yeniArraySpread.push(10)
+console.log(yeniArraySpread, ciftler)
+
+//? Tamamen Deep Kopyalama icin JSON.stringfy() - JSON.parse()
+
+//? Object copying
+
+// const firstObj = { a: 1, b: 2, c: 3 }
+// const secondObj = { a: 2, d: 3, c: 4 }
+
+// const copiedFirstObj = { ...firstObj }
+// console.log(copiedFirstObj)
+
+// copiedFirstObj.a = 44
+
+// //? nesting olmadigi icin deep copy gibi davranir.
+console.log(copiedFirstObj, firstObj)
+
+const combinedObjs = { ...firstObj, ...secondObj }
+console.log(combinedObjs)
+
+//? nested
+const sahislar = {
+    sahis1: {
+        name: "Can",
+        surname: "Canan",
+        dob: "1990",
+        job: "developer",
+        salary: "140000",
+        drivingLicense: true,
+    },
+    sahis2: {
+        name: "John",
+        surname: "Sweet",
+        dob: "1990",
+        job: "tester",
+        salary: "110000",
+        drivingLicense: false,
+    },
+    sahis3: {
+        name: "Steve",
+        surname: "Job",
+        dob: "2000",
+        job: "developer",
+        salary: "90000",
+        drivingLicense: true,
+    },
+}
+
+console.log("MAAS:", sahislar.sahis1.salary)
+
+//! FOR - IN
+//* for (key in object) {
+//*   // code block to be executed
+//* }
+
+for (let s in sahislar) {
+    // console.log(s)
+    // console.log(sahislar[s]) //? square bracket notasyon
+    console.log(sahislar[s].salary) //? square bracket notasyon
+}
+
+// /? Javascript'de Objeler default olarak iterable degildir.
+//? Ama for in ve for of donguleri ile itere edilebilirler.
+
+//? Objelerin key ve value'larini okumak icin built-in metotlar vardir.
+//? Bu mettotlar aslinda objelerin key ve/veya value'lari bir dizi olarak dondurur.
+console.log(Object.keys(sahislar))
+console.log(Object.values(sahislar))
+console.log(Object.entries(sahislar))
+
+console.log("****************")
+for (let key of Object.keys(sahislar)) {
+    console.log(key)
+}
